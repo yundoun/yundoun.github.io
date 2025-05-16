503632ef064242568bcfaf125f71215a66d41d85
윤도운/파인원 <dbsehdns12@pineone.com>
2025-05-16 10:54:15 +0900

    build : 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/yundoun.github.io/', // 슬래시 포함! 예: '/my-vite-app/'
+  plugins: [react()]
 })
